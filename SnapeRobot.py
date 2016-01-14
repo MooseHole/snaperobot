f7a5145addb33db6fd9f6719428ac346919969f3
Mike Schreiber <Mike.Schreiber@VGT.net>
2016-01-14 12:12:46 -0600

    Update test subreddit

--- SnapeRobot.py
+++ SnapeRobot.py
@@ -12,18 +12,23 @@ import xml.etree.ElementTree
 username = os.environ['REDDIT_USER']
 password = os.environ['REDDIT_PASS']
 #subreddit = "HarryPotter"
-subreddit = "Slytherin"
+subreddit = "Ghost_Of_Snape"
 triggerfile = "triggers.xml"
 debug = False
+r = null
+comments = null
+triggers = null
 
-# Login
-r = praw.Reddit('python:moosehole.Ghost_Of_Snape:v0.0.1 (by /u/Moose_Hole)'
-                'Url: https://github.com/MooseHole/SnapeRobot')
-r.login(username, password)
+# Logs in
+def login():
+	r = praw.Reddit('python:moosehole.Ghost_Of_Snape:v0.0.1 (by /u/Moose_Hole)'
+	                'Url: https://github.com/MooseHole/SnapeRobot')
+	r.login(username, password)
 
-# Load comments and triggers
-comments = r.get_comments(subreddit)
-triggers = xml.etree.ElementTree.parse(triggerfile).getroot()
+# Loads comments and triggers
+def load():
+	comments = r.get_comments(subreddit)
+	triggers = xml.etree.ElementTree.parse(triggerfile).getroot()
 
 
 # Prints a message if the debug flag is true
@@ -37,27 +42,32 @@ def respond(response):
 	printdebug(response)
 
 # Main loop
-for comment in comments:
-	printdebug(comment.author)
-	response = ""
+while True:
+	login()
+	load()
 
-	# Build responses to triggers
-	for trigger in triggers.findall('comment'):
-		if trigger.get('trigger') in comment.body:
-			response += trigger.get('response') + "  "
+	for comment in comments:
+		printdebug(comment.author)
+		response = ""
 
-	# If any response
-	if len(response) > 0:
-		# Skip if I already replied
-		replied = False
-		replies = r.get_submission(comment.permalink).comments
-		for reply in replies:
-			if reply.author == username:
-				replied = True
-				break
-		if replied:
-			continue
+		# Build responses to triggers
+		for trigger in triggers.findall('comment'):
+			if trigger.get('trigger') in comment.body:
+				response += trigger.get('response') + "  "
 
-		# Reply to the comment
-		respond(response)
+		# If any response
+		if len(response) > 0:
+			# Skip if I already replied
+			replied = False
+			replies = r.get_submission(comment.permalink).comments
+			for reply in replies:
+				if reply.author == username:
+					replied = True
+					break
+			if replied:
+				continue
 
+			# Reply to the comment
+			respond(response)
+
+	
