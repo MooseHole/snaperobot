c9b2891eab11327d22a45b6a923a1f73b59dafd1
Mike Schreiber <Mike.Schreiber@VGT.net>
2016-01-15 08:23:20 -0600

    Moving to submission ids

--- addsublinks.py
+++ addsublinks.py
@@ -8,65 +8,58 @@ import os
 import praw
 import requests
 import psycopg2
+import urlparse
 from random import randint
 import xml.etree.ElementTree
 
 triggerfile = "triggers.xml"
-#subreddit = "Ghost_Of_Snape"
-subreddit = "HarryPotter"
+subreddit = "Ghost_Of_Snape"
+#subreddit = "HarryPotter"
 
-username = "Ghost_Of_Snape"
-password = "fucker"
-debug = True
+username = os.environ['REDDIT_USER']
+password = os.environ['REDDIT_PASS']
+debug = os.environ['DEBUG_MODE']
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 
-
-# Logs in
-r = praw.Reddit('python:moosehole.Ghost_Of_Snape:v0.0.1 (by /u/Moose_Hole)'
-                'Url: https://github.com/MooseHole/SnapeRobo t')
-
-# Loads comments and triggers
-r.login(username, password)
-comments = r.get_comments(subreddit)
-triggerRoot = xml.etree.ElementTree.parse(triggerfile).getroot()
-triggers = triggerRoot.findall('trigger')
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 # Prints a message if the debug flag is true
 def printdebug(message):
 	if (debug):
 		print(message)
 
-# Responds to a comment
-def respond(response):
-#	comment.reply(response)
-	printdebug(response)
+cursor = conn.cursor()
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40pw1t\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40xe4d\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40xfnv\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40xi7l\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40xips\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40xzwf\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40zdrc\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40zsr7\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'40zx3w\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'410ccx\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'410o60\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'4114li\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'411n1e\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'411nbv\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'411le9\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'411trz\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'4128zu\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'412byh\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'412k9r\')')
+cursor.execute('INSERT INTO "Responded" (ID) VALUES (\'4130m7\')')
 
+conn.commit()
+cursor.close()
 
-response = ""
-ID = None
-
-# Main loop
-for comment in comments:
-	printdebug(comment.author)
-
-	if username == comment.author:
-		continue
-
-	# Skip if I already replied
-	# Build responses to triggers
-	for trigger in triggers:
-		if trigger.get('string') in comment.body:
-			printdebug(trigger.get('string'))
-			responses = trigger.findall('response')
-			responseIndex = randint(0, len(responses)-1)
-			response += responses[responseIndex].text + "  "
-			ID = comment.id
-			printdebug(comment.submission.id)
-			printdebug(comment.permalink)
-
-
-	if ID is not None:
-		break
-
-
-
+conn.close()
